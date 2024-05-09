edbec470cd3e3d2cd73a742602b54a20b2330852
WillssEdgar <willssedgar2123@gmail.com>
2024-05-09 19:51:36 -0400

    lazy change

--- lua/willssedgar/lazy.lua
+++ lua/willssedgar/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ { import = "willssedgar.plugins" }, { import = "willssedgar.plugins.lsp" } }, {
+require("lazy").setup("willssedgar.plugins", {
   checker = {
     enabled = true,
     notify = false,
