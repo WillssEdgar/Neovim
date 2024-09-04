1e4cf3d25754fa5f5d53b86fbc697860e78398d2
WillssEdgar <willssedgar2123@gmail.com>
2024-09-03 21:41:15 -0400

    nord theme

---
+++ lua/willssedgar/plugins/colors.lua
@@ -0,0 +1,18 @@
+return {
+  "EdenEast/nightfox.nvim",
+  config = function()
+    -- Optionally, customize Nightfox before loading the colorscheme
+    require("nightfox").setup({
+      options = {
+        -- Customize colors, styles, etc.
+        styles = {
+          comments = "italic", -- Italicize comments
+          keywords = "bold", -- Bold keywords
+        },
+      },
+    })
+
+    -- Load the nordfox colorscheme
+    vim.cmd("colorscheme nordfox")
+  end,
+}
